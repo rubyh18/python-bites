0306555fcb029861534bcec610955b4e14651a38
rubyh18 <rubyharjeet18@gmail.com>
2025-08-03 16:31:09 +0530

    Create range.py
    used range in for loop

---
+++ range.py
@@ -0,0 +1,13 @@
+#print numbers from 1 to 100.
+for i in range(1, 101):
+    print(i)
+
+#print numbers from 1000 to 1.
+for i in range(100, 0, -1):
+    print(i)  
+
+#print themultiplication table of anumber n.
+n = int(input("Enter number:"))
+for i in range(1, 11, 1):
+    print(n*i)  
+print("Done!")      
